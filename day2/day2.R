ffe3b8d41e3e0f2d6db9405bc82415c89189445d
mmanno <maxime.manno@inmano.com>
2024-12-01 19:54:02 +0100

    day 2 initial commit

---
+++ day2/day2.R
@@ -0,0 +1,24 @@
+##########
+# DAY 2
+##########
+
+########
+# Puzzle
+
+
+########
+# Libs
+library(tidyverse)
+library(data.table)
+
+########
+# input 
+myinput <- data.table::fread(file = "", sep = " ", header = F)
+
+########
+# Code part one
+
+########
+# Code part two
+
+
