36cc1ddb20c45e6fea59e58dbd0744d056a817ad
mmanno <maxime.manno@inmano.com>
2024-12-01 19:51:06 +0100

    Day1 done

--- day1/day1.R
+++ day1/day1.R
@@ -4,11 +4,41 @@
 
 ########
 # Puzzle
+# the input https://adventofcode.com/2024/day/1/input
+# What is the total distance between your lists?
 
 ########
 # Libs
 library(tidyverse)
+library(data.table)
 
 ########
-# Code
+# input 
+myinput <- data.table::fread(file = "day1/input.tsv", sep = " ", header = F, col.names = c("RL","LL"))
+
+########
+# Code part one
+
+# Reorder list and compute total diff
+myinput %>%
+  mutate(RL = RL[order(RL, decreasing = FALSE)]) %>%
+  mutate(LL = LL[order(LL, decreasing = FALSE)]) %>%
+  mutate(DIF = abs(RL - LL)) %>%
+  select(DIF) %>%
+  sum
+
+########
+# Code part two
+
+checknumberoftime <- function(number, list){
+  sum(list == number, na.rm = TRUE)
+}
+
+# change the number in the RL by the x time it is in the LL
+myinput %>%
+  rowwise() %>%
+  mutate(SM = RL * checknumberoftime(RL, myinput$LL)) %>%
+  select(SM) %>%
+  sum
+
 
